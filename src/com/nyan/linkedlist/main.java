09d544ed626a16fa952973f9bb63689d1bf9fa34
adytm404 <ogyaadyatma@gmail.com>
2023-03-26 20:23:09 +0700

    Initial commit

---
+++ src/com/nyan/linkedlist/main.java
@@ -0,0 +1,13 @@
+package com.nyan.linkedlist;
+
+public class main {
+    public static void main(String[] args) {
+        Link<String> myLink = new Link<String>();
+        myLink.add(4);
+        myLink.add(1);
+        myLink.add(2);
+        myLink.add(3);
+        myLink.add(5);
+        myLink.showData();
+    }
+}
